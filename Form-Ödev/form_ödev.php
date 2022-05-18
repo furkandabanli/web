b52f9d564e2421595b8dbea882b75a55ed319115
furkan <dabanlifurkan@gmail.com>
2022-05-18 12:59:57 +0300

    Ödev Yapıldı.

---
+++ Form-Ödev/form_ödev.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form-Ödev</title>
+</head>
+<body>
+    
+<form action="/action_page.php">
+  <label for="fname">İsim Soyisim:</label><br>
+  <input type="text" id="fname" name="fname" ><br>
+  <label for="fname">Email:</label><br>
+  <input type="text" id="fname" name="email" ><br>
+  <label for="fname">Telefon:</label><br>
+  <input type="text" id="fname" name="phone" ><br>
+  <label for="fname">Cinsiyet:</label><br>
+  <input type="radio" id="html" name="male" value="">
+  <label for="html">Erkek</label>
+<input type="radio" id="html" name="male" value="">
+  <label for="html">Kadın</label><br>
+<label for="fname">Ders:</label><br>
+<input type="radio" id="html" name="d" value="">
+  <label for="html">Yazılım Mimarileri</label><br>
+<input type="radio" id="html" name="d" value="">
+  <label for="html">Web Programlama</label><br>
+<input type="radio" id="html" name="d" value="">
+  <label for="html">Sunucu İşletim Sistemleri</label><br>
+  <label for="fname">Notunuz:</label><br>
+  <input type="text" id="fname" name="score" ><br><br>
+  <input type="submit" value="Submit">
+
+</form> 
+</body>
+</html>
